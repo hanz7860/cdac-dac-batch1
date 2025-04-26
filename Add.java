25815f114ccc2d14d36e327fbf05eb892b3844bb
Hanzala Khan <amaanwarsi786.aw@gmail.com>
2025-04-26 17:00:48 +0530

    Adding Java Program

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add{
+	public static void main(String args[]){
+		Integer a = 512;
+		Integer b = 258;
+
+		Integer sum = a + b;
+
+		System.out.println("Addition of " +a +" and " +b +" = " +sum);
+	}
+}
